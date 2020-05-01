3af83cca05a0ca583a9e0501ae28bdbcd9876a86
Larisa <l.demidova@elama.ru>
2020-05-01 21:52:49 +0300

    Add /users

--- backend/src/app/routes/controller/v1/index.ts
+++ backend/src/app/routes/controller/v1/index.ts
@@ -1,4 +1,5 @@
 import Router from 'express-promise-router'
+import getUsers from './users/getUsers'
 
 const router = Router()
 
@@ -6,5 +7,7 @@ router.use((request, response, next) => {
   response.locals.version = 'v1'
   next()
 })
+
+router.get('/users', getUsers)
 
 export default router
